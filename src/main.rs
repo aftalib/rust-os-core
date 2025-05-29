b935d9e84f63e5891b6b7774c671af993455e04d
aftalib <mehdi.painchault@outlook.fr>
2025-05-29 23:56:53 +0200

    build writer and print function using lazy_static and spinlocks

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,30 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello, world!\n";
+// static test: &[u8] = b"testrustos!\n";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    /*  let vga_buffer = 0xb8000 as *mut u8;
+    println!("hello{}", "!");
+    panic!("oh no");
     
-    for (i, &byte) in HELLO.iter().enumerate() {
+    /* 
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("test").unwrap();
+    write!(vga_buffer::WRITER.lock(), "some numb {} {}", 55, 1.2351).unwrap();
+
+
+     vga_buffer::print_something();
+
+
+     let vga_buffer = 0xb8000 as *mut u8;
+    
+    for (i, &byte) in test.iter().enumerate() {
         unsafe {
             // Write the character to the VGA buffer
             *vga_buffer.offset(i as isize * 2) = byte;
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -19,6 +19,7 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+    use lazy_static::lazy_static;
 
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -41,9 +42,10 @@ struct ScreenChar {
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
+use volatile::Volatile;
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -63,10 +65,10 @@ impl Writer {
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
@@ -81,11 +83,53 @@ impl Writer {
         }
     }
     fn new_line (&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row-1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
 
+    }
+
+    fn clear_row(&mut self, row : usize) {
+        let blank = ScreenChar {
+            ascii_character : b' ',
+            color_code : self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
+
+    
+}
+
+use spin::Mutex;
+
+lazy_static! {
+        pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+            column_position: 0,
+            color_code: ColorCode::new(Color::Yellow, Color::Black),
+            buffer: unsafe {&mut *(0xb8000 as *mut Buffer)},
+        });
+    }
+
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
     }
 }
 
 pub fn print_something() {
+        use core::fmt::Write;
         let mut writer = Writer {
             column_position : 0,
             color_code : ColorCode::new(Color::Yellow, Color::Black),
@@ -95,4 +139,22 @@ pub fn print_something() {
         writer.write_byte(b'H');
         writer.write_string("eeeellloo");
         writer.write_string("Wörld!");
+        write!(writer, "The numbers : {} and {} ", 45, 1.0/3.0).unwrap();
     }
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
